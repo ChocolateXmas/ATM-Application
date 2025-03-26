d6d37563fd48128ea395a18b071a2978a859e7bf
Chocolate_Starfish <alex.beigel@gmail.com>
2025-03-26 18:44:31 +0200

    feat(main.py): Added shell UI welcome screen using "curses" python lib

--- src/main.py
+++ src/main.py
@@ -1,7 +1,24 @@
 #!/usr/bin/env python3
 
-def main():
-	print("Hello!")
+import curses
+
+def main(stdscr):
+	curses.start_color()
+	curses.init_pair(1, curses.COLOR_WHITE, curses.COLOR_BLUE)
+	stdscr.bkgd(' ', curses.color_pair(1))
+	stdscr.clear()
+	height, width = stdscr.getmaxyx()
+	title = "Atm Application"
+	title_y = height // 2
+	title_x = (width - len(title)) // 2
+	stdscr.addstr(title_y, title_x, title, curses.color_pair(1) | curses.A_BOLD)
+	win = curses.newwin(12, 40, 20, 20)
+	win.box()
+	win.addstr(5, 10, "Inside a box!", curses.color_pair(1))
+	win.refresh()
+	stdscr.refresh()
+	stdscr.getch()
+# END main
 
 if __name__ == "__main__":
-	main()
+	curses.wrapper(main)
