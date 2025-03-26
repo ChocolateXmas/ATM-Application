1ec223c62b4eb7c20e897e3cdeef9c394f665fba
Chocolate_Starfish <alex.beigel@gmail.com>
2025-03-26 19:42:01 +0200

    feat: Added atm.py for atm methods and logic.

---
+++ src/atm.py
@@ -0,0 +1,19 @@
+class Atm:
+    def __init__(self):
+        pass
+    
+    def deposit(self, amount: float):
+        # TODO: Add deposit logic
+        pass
+
+    def withdraw(self, amount: float):
+        #TODO: Add withdraw logic
+        pass
+
+    def getBalance(self) -> float:
+        #TODO: Add getBalance logic
+        pass
+
+    def quit(self):
+        break
+# END Atm
