81a6b7ca1c3776ab44c0f878d24729f417415b67
Chocolate_Starfish <alex.beigel@gmail.com>
2025-03-28 15:36:59 +0300

    docs(atm.py): Added clearer function docs.
    - feat(atm.py): Added a functino to check if user is logged in for better code reusability.

--- src/atm.py
+++ src/atm.py
@@ -14,8 +14,22 @@ class Atm:
         self.__loggedIn = False # True if user is logged in, False otherwise
     # END __init__
 
-    # True if the user exists and the pin is correct and sets user's account info, False otherwise
+    """
+        Returns True if user is logged in.
+        False otherwise.
+    """
+    def __is_logged_in(self) -> bool: 
+        return self.__isLogged
+    # END __is_logged_in
+
+    """
+         False if user already logged in.
+        Returns True if the user exists and the pin is correct.
+        False otherwise.
+    """
     def login(self, fullName: str, pin: str) -> bool:
+        if self.__is_logged_in():
+            return False
         if fullname in users:
             if hash(users[fullName]["pin"]) == hash(pin):
                 self.__userName = fullName
@@ -27,7 +41,13 @@ class Atm:
     # END login
 
     # Sets user's account info to None and logs out the user
-    def logOut(self):
+    """
+        Raises a ValueError if the user is not logged in.
+        Continues regularly if the user log out successful, and sets all info to None.
+    """
+    def logOut(self) -> None:
+        if not self.__is_logged_in():
+            raise ValueError("User is not logged in")
         self.__userName = None
         self.__pinCode = None
         self.__balance = None
@@ -41,10 +61,15 @@ class Atm:
 
     # Raises a ValueError if the user is not logged in
     def __ensure_logged_in(self) -> None: 
-        if not self.__loggedIn:
+        if not self.__is_logged_in():
             raise ValueError("User is not logged in")
     # END ensure_logged_in
 
+    """
+        Raises a ValueError if the user is not logged in.
+        Raises a ValueError if the amount is not a multiple of 20, 50 or 100.
+        Adds the amount to the user's balance.
+    """
     def deposit(self, amount: float) -> None:
         self.__ensure_logged_in()
         if self.is_valid_amount(amount):
@@ -54,6 +79,12 @@ class Atm:
             raise ValueError("Amount must be a multiple of 20, 50 or 100")
     # END deposit
 
+    """
+        Raises a ValueError if the user is not logged in.
+        Raises a ValueError if the amount is not a multiple of 20, 50 or 100.
+        Raises a ValueError if the amount is greater than the user's balance.
+        Withdraws the amount from the user's balance.
+    """
     def withdraw(self, amount: float) -> None:
         self.__ensure_logged_in()
         if amount > self.__balance:
