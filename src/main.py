e38b81356f322c8aa6ba2ed33352266104311e73
Chocolate_Starfish <alex.beigel@gmail.com>
2025-03-28 22:32:17 +0300

    feat(src/main.py): Added Menu class and methods.
    - feat: init and del functions added for starting and ending the menu.
    - feat: set colors for terminal and input box colors.

--- src/main.py
+++ src/main.py
@@ -1,30 +1,44 @@
 #!/usr/bin/env python3
 
 import curses
+from curses.textpad import Textbox, rectangle
 from atm import Atm
 
+class Menu:
+	def __init__(self, stdscr, title="Menu"):
+		self.__title  = title
 
+		# Initialize the ATM application Colors
+		curses.init_pair(1, curses.COLOR_WHITE, curses.COLOR_BLUE)
+		self.__terminal_color = curses.color_pair(1) # Set the terminal color
+		
+		curses.init_pair(2, curses.COLOR_BLACK, curses.COLOR_WHITE)
+		self.__input_color = curses.color_pair(2) # Set the input color
+	# END __init__
+
+	def start(self):
+		
+		pass
+	# END start 	
+
+	def end(self):
+		# Clean up the curses window
+		curses.endwin()
+	# END end
+
+	def login(self):
+		pass
+	# END login
+
+	def __del__(self):
+		# Clean up the curses window
+		curses.endwin()
+	# END __del__
+# END Menu
 
 def main(stdscr):
-	i = 0
-	while True:
-		curses.start_color()
-		curses.init_pair(1, curses.COLOR_WHITE, curses.COLOR_BLUE)
-		stdscr.bkgd(' ', curses.color_pair(1))
-		stdscr.clear()
-		height, width = stdscr.getmaxyx()
-		title = f"Atm Application {i}"
-		i += 1
-		title_y = height // 2
-		title_x = (width - len(title)) // 2
-		stdscr.addstr(title_y, title_x, title, curses.color_pair(1) | curses.A_BOLD)
-		win = curses.newwin(12, 40, 20, 20)
-		win.box()
-		win.addstr(5, 10, "Inside a box!", curses.color_pair(1))
-		win.refresh()
-		stdscr.refresh()
-		stdscr.getch()
-	# END while
+	menu = Menu(stdscr, "ATM Application")
+	menu.start()
 # END main
 
 if __name__ == "__main__":
