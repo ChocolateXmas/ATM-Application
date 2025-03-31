e715794b69e536c782dc1bf25e3e4e1134523341
Chocolate_Starfish <alex.beigel@gmail.com>
2025-03-31 23:06:31 +0300

    feat(src/main.py): Added __show_deposit_inputbox() and __show_withdraw_inputbox() for better transaction handling.
    - Implemented both new fucntions in __<withdraw/deposit>_screen() for better code structure.
    - Introduced __show_deposit_inputbox() and __show_withdraw_inputbox() to improve deposit and withdrawal input handling.
    - Improved function documentation for __show_<deposit/pincode>_inputbox to enhance clarity:
        (-) Updated descriptions to better explain the logic behind input box positioning.
        (-) Clarified the `formerElement` parameter and its effect on UI layout.

--- src/main.py
+++ src/main.py
@@ -32,12 +32,23 @@ class Menu:
 			"p": {"msg" : "Press button p to Change PIN_CODE", "function" : self.__change_pin_screen},
 			"r": {"msg" : "Press button r to Get a RECIPE", "function" : self.__recipe_screen}
 		}
+
+		self.__withdraw_amounts = [50, 100, 150, 300]
+		"""
+			self.__withdraw_options : dict()
+				\ * -> 	(option: amount) ~ (str: int[amount] / str["Other"])
+		"""
+		# Dict comphrehension for options to withdraw ( 1 - 50, 2 - 100, etc ...)
+		self.__withdraw_options = { str(option + 1) : (self.__withdraw_amounts[option] if option < len(self.__withdraw_amounts) else "Other")
+									for option in range(0, len(self.__withdraw_amounts) + 1) }
 	# END __init__
 
 	def __display_title(self):
 		# Display the title at the top center
 		self.stdscr.addstr(self.__title_y, self.__title_x, self.__title, self.__terminal_color | curses.A_BOLD)
 	# END __display_title	
+
+
 
 	def __display_greet_title(self):
 		self.stdscr.addstr(self.__greet_title_y, self.__greet_title_x, self.__greet_title, self.__terminal_color | curses.A_BOLD)
@@ -62,18 +73,143 @@ class Menu:
 		# Clean up the curses window
 		curses.endwin()
 	# END end
+	
+	"""
+		Gets the wanted Y-coordinate for Deposit input box location.
+		formerElement:bool is True if we have a former element above the Deposit input box - that way the input box will be below it with an empty break line,
+							  False otherwise.
+		* Deposit amount can be only a multiplier of 20, 50, 100 !
+
+		Returns:
+			int: Wanted Deposit amount if casting was successful, otherwise returns 0.
+	"""
+	def __show_deposit_inputbox(self, desposit_y, formerElement=False) -> int:
+		deposit_prompt = "Enter Deposit Amount: "
+		max_deposit_length = 10
+		desposit_y += 2 if formerElement else 0
+		deposit_x = self.__get_middle_x(deposit_prompt + " " * max_deposit_length)
+		# Input box for Deposit
+		self.stdscr.addstr(desposit_y, deposit_x, deposit_prompt, self.__input_color)
+		deposit_win = curses.newwin(1, max_deposit_length, desposit_y, deposit_x + len(deposit_prompt))
+		curses.textpad.rectangle(self.stdscr, desposit_y - 1, deposit_x + len(deposit_prompt) - 1,
+									desposit_y + 1, deposit_x + len(deposit_prompt) + max_deposit_length)
+		self.stdscr.refresh()
+		# Deposit amount value input is a string now, but will be converted to int later
+		deposit_amount = ""
+		while True:
+			key = deposit_win.getch()
+			if key in range(48, 58): # Check if the key is a digit (ASCII 48-57)
+				if len(deposit_amount) < max_deposit_length - 1:
+					deposit_amount += chr(key)
+					deposit_win.addch(chr(key))
+				# END if 
+			# END if
+			elif key == curses.KEY_BACKSPACE or key == 127: # Handle backspace
+				if len(deposit_amount) > 0:
+					deposit_amount = deposit_amount[:-1]
+					y, x = deposit_win.getyx()
+					deposit_win.delch(y, x - 1)
+				# END if
+			# END elif
+			elif key == curses.KEY_ENTER or key == 10 or key == 13: # Enter key
+				if len(deposit_amount) == 0:
+					empty_deposit_amount_msg = "PIN must be 4 digits. Please try again."
+					self.stdscr.addstr(desposit_y + 2, self.__get_middle_x(empty_deposit_amount_msg), empty_deposit_amount_msg, self.__terminal_color | curses.A_BOLD | curses.A_STANDOUT | curses.A_UNDERLINE)
+					self.stdscr.refresh()
+				# END if
+				break
+			# END elif
+		# END while
+		self.__clear_line(desposit_y + 2)
+		self.stdscr.refresh()
+		try:
+			return int(deposit_amount)
+		except:
+			return 0
+	# END __show_deposit_inputbox
+	
+	"""
+		Gets the wanted Y-coordinate for Withdraw input box location.
+		formerElement:bool is True if we have a former element above the Withdraw input box - that way the input box will be below it with an empty break line,
+							  False otherwise.
+		* Withdraw amount can be only a multiplier of 20, 50, 100 !
+
+		Returns:
+			int: Wanted Withdraw amount if casting was successful, otherwise returns 0.
+	"""
+	def __show_withdraw_options(self, withdraw_y, formerElement=False) -> int:
+		# This function assumes the options were already printed to the user, and now it's destiny it to only get the option to withdraw
+		withdraw_prompt = "Choose Option: "
+		withdraw_y += 2 if formerElement else 0
+		withdraw_x = self.__get_middle_x(withdraw_prompt)
+		other_amount_prompt = "Enter amount: "
+		max_other_amount_length = 10
+		other_y = withdraw_y + 2 # Y Coordinate for "Enter amount: " message
+		other_x = self.__get_middle_x(other_amount_prompt + " " * max_other_amount_length)
+		other_empty_amount_y = other_y + 2 # Y Coordinate for Empty Amount input message
+		self.stdscr.addstr(withdraw_y, withdraw_x, withdraw_prompt, self.__terminal_color | curses.A_BOLD)
+		self.stdscr.refresh()
+		other_amount = ""
+		selection_done = False # If Other amount was entered, end the outer while loop and return a value
+		while not selection_done:
+			chosen_option = self.stdscr.getch()
+			if chr(chosen_option) in self.__withdraw_options.keys():
+				if isinstance(self.__withdraw_options[ chr(chosen_option) ], int): # Means it's a number of NIS
+					return self.__withdraw_options[ chr(chosen_option) ]
+				# END if
+				else: # "Other" amount to withdraw
+					self.stdscr.addstr(other_y, other_x, other_amount_prompt, self.__terminal_color | curses.A_BOLD)
+					other_win = curses.newwin(1, max_other_amount_length, other_y, other_x + len(other_amount_prompt))
+					curses.textpad.rectangle(self.stdscr, other_y - 1, other_x + len(other_amount_prompt) - 1,
+														  other_y +1 , other_x + len(other_amount_prompt) + max_other_amount_length + 1)
+					self.stdscr.refresh()
+					while True:
+						key = other_win.getch()
+						if key in range(48, 58): # Check if the key is a digit (ASCII 48-57)
+							if len(other_amount) == 0 and chr(key) == '0': # disallow "0" as a first char of amount
+								continue
+							# END if
+							elif len(other_amount) < max_other_amount_length - 1:
+								other_amount += chr(key)
+								other_win.addch( chr(key) )
+							# END elif 
+						# END if
+						elif key == curses.KEY_BACKSPACE or key == 127: # Handle backspace
+							if len(other_amount) > 0:
+								other_amount = other_amount[:-1]
+								y, x = other_win.getyx()
+								other_win.delch(y, x - 1)
+							# END if
+						# END elif
+						elif key == curses.KEY_ENTER or key == 10 or key == 13: # Enter key
+							if len(other_amount) == 0: raise ValueError("Amount can't be empty")
+							else: selection_done = True; break;
+						# END elif
+					# END while
+				# END else
+			# END if
+			else:
+				raise ValueError("No Option Selected!")
+			# END if
+		# END while
+		try: 
+			return int(other_amount)
+		except ValueError: 
+			return 0
+	# END __show_withdraw_options
 
 	"""
-		Gets the Y-coordinate of a former element for displaying the PINCODE input box below it accordingly.
-		former:bool is True if we have a former element above the PINCODE input box, False otherwise
+		Gets the Y-coordinate for PINCODE input box location.
+		formerElement:bool is True if we have a former element above the PINCODE input box - that way the input box will be below it with an empty break line,
+							  False otherwise.
 
 		Returns:
 			str: The entered PIN code as a string.
 	"""
-	def __show_pincode_inputbox(self, pin_y, former=False) -> str:
+	def __show_pincode_inputbox(self, pin_y, formerElement=False) -> str:
 		pin_prompt = "Enter PIN (4 digits): "
 		max_pin_length = 14
-		pin_y += 2 if former else 0
+		pin_y += 2 if formerElement else 0
 		pin_x = self.__get_middle_x(pin_prompt + " " * max_pin_length)
 		# Input for PIN (hidden with *)
 		self.stdscr.addstr(pin_y, pin_x, pin_prompt, self.__input_color)
@@ -141,7 +277,7 @@ class Menu:
 			self.stdscr.addstr(username_y, welcome_x, welcome_msg, self.__terminal_color | curses.A_BOLD)
 			self.stdscr.refresh()
 
-			pin = self.__show_pincode_inputbox(username_y , True)
+			pin = self.__show_pincode_inputbox(username_y , formerElement=True)
 
 			# Check if the username and PIN are correct, and set the user info
 			if not self.__atm.login(username, pin):
@@ -179,14 +315,86 @@ class Menu:
 			# END if
 		# END while
 	# END __greet
-
+	
 	def __deposit_screen(self):
 		# self.__atm.deposit
-		pass
+		middle_y = (self.height // 2) - 3 # minus 3 because we have 3 lines to show (deposit_msg | error_msg), the input box, and one empty break line between them
+		try:
+			self.stdscr.clear() 
+			deposit_msg = "How much would you like to Deposit?"
+			self.stdscr.addstr(middle_y, self.__get_middle_x(deposit_msg), deposit_msg, self.__terminal_color | curses.A_BOLD | curses.A_STANDOUT)
+			self.stdscr.refresh()
+			deposit_amount = self.__show_deposit_inputbox(middle_y, formerElement=True)
+			self.__atm.deposit(deposit_amount)
+			"""
+				Raises a ValueError if the user is not logged in.
+				Raises a ValueError if the amount is not a multiple of 20, 50 or 100.
+				otherwise, Adds the amount to the user's balance.
+			"""
+			middle_y += 2 # 1 line for new pin msg + input box takes 2 lines + 1 empty break line
+			self.__clear_line( *range(middle_y-1, middle_y + 1 + 1) )
+			deposit_success_msg = f"{deposit_amount} NIS successfuly deposited!"
+			self.stdscr.addstr(middle_y, self.__get_middle_x(deposit_success_msg), deposit_success_msg, self.__terminal_color | curses.A_BOLD | curses.A_STANDOUT)
+			self.stdscr.refresh()
+			middle_y += 2
+		# END try
+		except ValueError as ve: 
+			error_msg = str(ve)
+			self.stdscr.clear()
+			self.stdscr.addstr(middle_y, self.__get_middle_x(error_msg), error_msg, self.__terminal_color | curses.A_BOLD | curses.A_STANDOUT)
+			self.stdscr.refresh()
+		# END except
+		middle_y += 2 # 1 line for new pin msg + input box takes 2 lines + 1 empty break line
+		deposit_exit_msg = "Press any key to get BACK"
+		self.stdscr.addstr(middle_y, self.__get_middle_x(deposit_exit_msg), deposit_exit_msg, self.__terminal_color | curses.A_BOLD | curses.A_STANDOUT)
+		self.stdscr.refresh()
+		self.stdscr.getch()
 	# END __deposit_screen
 
 	def __withdraw_screen(self):
 		# __atm.withdraw
+		withdraw_options_lines_amount = len(self.__withdraw_options)
+		middle_y = (self.height // 2) - (withdraw_options_lines_amount + 2) # amount of withdraw options is the amount of rows needed + 1 for the withdraw_msg + 1 empty break line
+		try:
+			self.stdscr.clear() 
+			withdraw_msg = "How much would you like to Withdraw?"
+			self.stdscr.addstr(middle_y, self.__get_middle_x(withdraw_msg), withdraw_msg, self.__terminal_color | curses.A_BOLD)
+			middle_y += 2
+			for option, amount in self.__withdraw_options.items():
+				line = f"{option} - {amount}"
+				self.stdscr.addstr(middle_y, self.__get_middle_x(line), line, self.__terminal_color | curses.A_BOLD | curses.A_STANDOUT)
+				middle_y += 1
+			# END for
+			self.stdscr.refresh()
+			withdraw_amount = self.__show_withdraw_options(middle_y, formerElement=True)
+			self.__atm.withdraw(withdraw_amount)
+			"""
+				Raises a ValueError if the user is not logged in.
+				Raises a ValueError if the amount is not a multiple of 20, 50 or 100.
+				Raises a ValueError if the amount is greater than the user's balance.
+				Otherwise, Withdraws the amount from the user's balance.
+			"""
+			# middle_y += 2 # 1 line for new pin msg + input box takes 2 lines + 1 empty break line
+			# self.__clear_line( *range(middle_y-1, middle_y + 1 + 1) )
+			# deposit_success_msg = f"{deposit_amount} NIS successfuly deposited!"
+			# self.stdscr.addstr(middle_y, self.__get_middle_x(deposit_success_msg), deposit_success_msg, self.__terminal_color | curses.A_BOLD | curses.A_STANDOUT)
+			# self.stdscr.refresh()
+			middle_y += 2
+			withdraw_success_msg = f"{withdraw_amount} NIS successfuly withdrawed!"
+			self.stdscr.addstr(middle_y, self.__get_middle_x(withdraw_success_msg), withdraw_success_msg, self.__terminal_color | curses.A_BOLD | curses.A_STANDOUT)
+			self.stdscr.refresh()
+		# END try
+		except ValueError as ve: 
+			error_msg = str(ve)
+			self.stdscr.clear()
+			self.stdscr.addstr(middle_y, self.__get_middle_x(error_msg), error_msg, self.__terminal_color | curses.A_BOLD | curses.A_STANDOUT)
+			self.stdscr.refresh()
+		# END except
+		middle_y += 2 # 1 line for new pin msg + input box takes 2 lines + 1 empty break line
+		deposit_exit_msg = "Press any key to get BACK"
+		self.stdscr.addstr(middle_y, self.__get_middle_x(deposit_exit_msg), deposit_exit_msg, self.__terminal_color | curses.A_BOLD | curses.A_STANDOUT)
+		self.stdscr.refresh()
+		self.stdscr.getch()
 		pass
 	# END __withdraw_screen
 
@@ -217,23 +425,14 @@ class Menu:
 		self.stdscr.getch()
 	# END __logout
 
-	"""
-		def change_pin(self, newPin: str) -> None:
-			if not self.is_logged_in():
-				raise ValueError("User is not logged in")
-			# END if
-			users[self.__userName]["pin"].set_pin(newPin)
-		# END change_pin
-	"""
 	def __change_pin_screen(self):
-		# __atm.change_pin
-		middle_y = (self.height // 2) - 3 # minus 3 because we have 3 lines to show (logout_msg | error_msg), and one empty break line between them
+		middle_y = (self.height // 2) - 3 # minus 3 because we have 3 lines to show (new_pin_msg | error_msg), the input box, and one empty break line between them
 		try:
 			self.stdscr.clear() 
 			new_pin_msg = f"Hello {str(self.__atm.get_user_name())}, Enter NEW PINCODE"
 			self.stdscr.addstr(middle_y, self.__get_middle_x(new_pin_msg), new_pin_msg, self.__terminal_color | curses.A_BOLD | curses.A_STANDOUT)
 			self.stdscr.refresh()
-			new_pin = self.__show_pincode_inputbox(middle_y , True)
+			new_pin = self.__show_pincode_inputbox(middle_y , formerElement=True)
 			self.__atm.change_pin(new_pin)
 		# END try
 		except ValueError as ve: 
@@ -242,7 +441,7 @@ class Menu:
 			self.stdscr.addstr(middle_y, self.__get_middle_x(error_msg), error_msg, self.__terminal_color | curses.A_BOLD | curses.A_STANDOUT)
 			self.stdscr.refresh()
 		# END except
-		middle_y += 4
+		middle_y += 4 # 1 line for new pin msg + input box takes 2 lines + 1 empty break line
 		new_pin_exit_msg = "PINCODE changed succesfuly! Press any key to get BACK"
 		self.stdscr.addstr(middle_y + 2, self.__get_middle_x(new_pin_exit_msg), new_pin_exit_msg, self.__terminal_color | curses.A_BOLD | curses.A_STANDOUT)
 		self.stdscr.refresh()
@@ -250,7 +449,6 @@ class Menu:
 	# END __change_pin_screen
 
 	def __recipe_screen(self):
-		# __atm.get_recipe
 		self.stdscr.clear()
 		recipe_msg = self.__atm.get_recipe() # List of lines of messages
 		middle_y = (self.height // 2) - len(recipe_msg)
@@ -262,7 +460,6 @@ class Menu:
 		self.stdscr.addstr(middle_y + 1, self.__get_middle_x(recipe_exit_msg), recipe_exit_msg, self.__terminal_color | curses.A_BOLD | curses.A_STANDOUT)
 		self.stdscr.refresh()
 		self.stdscr.getch()
-		# pass
 	# END __recipe_screen
 
 	"""
